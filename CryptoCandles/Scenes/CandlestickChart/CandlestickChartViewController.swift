37db45d3586cf137b3e3dc812cfcf908016356c7
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 17:44:17 +0200

    Build CandlestickChart UI

--- CryptoCandles/Scenes/CandlestickChart/CandlestickChartViewController.swift
+++ CryptoCandles/Scenes/CandlestickChart/CandlestickChartViewController.swift
@@ -1,12 +1,11 @@
 //
-//  ViewController.swift
+//  CandlestickChartViewController.swift
 //  CryptoCandles
 //
 //  Created by Nada Yehia Dawoud on 21/10/2022.
 //
 
 import UIKit
-
 class CandlestickChartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
