1985e4aee3509ccf59e206f7b3bc77149a1703ed
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 17:53:01 +0200

    Create CandlestickChartPresenter

---
+++ CryptoCandles/Scenes/CandlestickChart/CandlestickChartPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  CandlestickChartPresenter.swift
+//  CryptoCandles
+//
+//  Created by Nada Yehia Dawoud on 29/10/2022.
+//
+
+import Foundation
+
+protocol CandlestickChartPresentationLogic {
+    func presentFetchedCandlesticks(_ candlesticks: Candlesticks)
+}
+
+class CandlestickChartPresenter: CandlestickChartPresentationLogic {
+    
+    func presentFetchedCandlesticks(_ candlesticks: Candlesticks) {
+        // TODO: Present Fetched Candlesticks
+    }
+}
