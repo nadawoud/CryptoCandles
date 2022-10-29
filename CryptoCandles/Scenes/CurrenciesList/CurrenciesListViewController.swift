b999ef1a5d9b3dc6efbf195b8b2bc93a15a54bb1
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 21:50:51 +0200

    Create CurrenciesListViewController

---
+++ CryptoCandles/Scenes/CurrenciesList/CurrenciesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CurrenciesListViewController.swift
+//  CryptoCandles
+//
+//  Created by Nada Yehia Dawoud on 29/10/2022.
+//
+
+import UIKit
+
+protocol CurrenciesListDisplayLogic {
+    func displayCurrencies(_ currencies: [CryptoCurrency])
+}
+
+class CurrenciesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
