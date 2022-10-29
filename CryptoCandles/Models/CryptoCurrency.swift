dad2eea4de5403b6864fafefd6476e4c1f7e7fc2
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 21:49:39 +0200

    Move CryptoCurrency to its own file

---
+++ CryptoCandles/Models/CryptoCurrency.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCandles
+//
+//  Created by Nada Yehia Dawoud on 29/10/2022.
+//
+
+import Foundation
+
+enum CryptoCurrency: String, CaseIterable {
+    case btc = "BTCUSDT"
+    case ltc = "LTCUSDT"
+    case eth = "ETHUSDT"
+}
