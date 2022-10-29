aa10af6903735c2deddcc06ace535a56955c6f75
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 17:27:16 +0200

    Rename ViewController CandlestickChartViewController

--- CryptoCandles/CandlestickChartViewController.swift
+++ CryptoCandles/CandlestickChartViewController.swift
@@ -7,13 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CandlestickChartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
