8a0e4f42ff9d21d39ba5ac569568ce73a2c17649
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-10-29 21:48:47 +0200

    Extend UIView

---
+++ CryptoCandles/Extensions/UIView+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Ext.swift
+//  CryptoCandles
+//
+//  Created by Nada Yehia Dawoud on 29/10/2022.
+//
+
+import Foundation
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get { return layer.cornerRadius }
+    }
+}
