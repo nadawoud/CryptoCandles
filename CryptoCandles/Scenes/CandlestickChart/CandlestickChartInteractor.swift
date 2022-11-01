3546f7e3cc7ec7f95f42dbae463cd8bfd5c61cd2
Nada Yehia Dawoud <10497712+nadawoud@users.noreply.github.com>
2022-11-01 10:56:22 +0200

    Create NetworkService and CryptoCandlesService to make it easier to mock

--- CryptoCandles/Scenes/CandlestickChart/CandlestickChartInteractor.swift
+++ CryptoCandles/Scenes/CandlestickChart/CandlestickChartInteractor.swift
@@ -11,19 +11,30 @@ protocol CandlestickChartBusinessLogic {
     func fetchChartData(forCurrency currency: CryptoCurrency)
 }
 
-class CandlestickChartInteractor: CandlestickChartBusinessLogic {
+protocol CryptoCandlesService {
+    func fetchCandlesticks(forCurrency currency: CryptoCurrency, completion: @escaping (Result<Candlesticks, NetworkError>) -> Void)
+}
+
+class CandlestickChartInteractor: CandlestickChartBusinessLogic, CryptoCandlesService {
+    
     var presenter: CandlestickChartPresentationLogic?
+    
+    func fetchCandlesticks(forCurrency currency: CryptoCurrency, completion: @escaping (Result<Candlesticks, NetworkError>) -> Void) {
+        let networkManager = NetworkManager()
+       
+        return networkManager.request(endpoint: CryptoCandleEndpoint.getCandlestickChartData(currency: currency), completion: completion)
+    }
     
     func fetchChartData(forCurrency currency: CryptoCurrency) {
         
-        NetworkManager.request(endpoint: CryptoCandleEndpoint.getCandlestickChartData(currency: currency)) { [weak self] (result: Result<Candlesticks, NetworkError>) in
+        fetchCandlesticks(forCurrency: currency) { [weak self] (result: Result<Candlesticks, NetworkError>) in
             guard let self = self else { return }
             
             switch result {
             case .success(let candlesticks):
                 print("Candlesticks: \(candlesticks)")
                 self.presenter?.presentFetchedCandlesticks(candlesticks)
-            
+                
             case .failure(let error):
                 print(error)
                 self.presenter?.presentFetchingError()
